9acfe7f9a30e2d841efc4c68f1ae0f123cbf3ff3
cecilelecerf <c.lecerf.cecile@gmail.com>
2023-06-02 13:02:17 +0200

    cta composant

---
+++ components/CTA.vue
@@ -0,0 +1,52 @@
+<script></script>
+
+<template>
+    <div class="button primary">
+      <a href="">Test du bouton</a>
+    </div>
+</template>
+
+<style lang="scss">
+.button {
+    padding: 17px 30px;
+    border-radius: 4px;
+    width: fit-content;
+    height: fit-content;
+    border: solid 2px $main;
+    transition: all 300ms ease-out;
+    color: $white;
+    @media screen and (max-width: 600px) {
+        &{
+            padding: 11px 17px;
+        }
+    }
+    a{
+        text-align: center;
+    }
+    &.primary{
+        background-color: $main;
+        &:hover{
+            color: $main;
+            background-color: transparent;
+        }
+    }
+    &.secondary{
+        background-color: $white;
+        color: $main;
+        &:hover{
+            border-color: $white;
+            background-color: $main;
+            color: $white;
+        }
+    }
+    &.outline{
+        border-color: $white;
+        &:hover{
+            background-color: $white;
+            border-color: $main;
+            color: $main;
+        }
+    }
+} 
+
+</style>
